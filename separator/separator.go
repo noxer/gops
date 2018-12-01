16650368d06dc5f09c65b4c2a67c0f38cc484989
Tim Scheuermann <tim@plusmid.net>
2018-12-01 18:08:01 +0100

    Comments for the separator package

--- separator/separator.go
+++ separator/separator.go
@@ -7,6 +7,7 @@ import (
 	"github.com/noxer/gops/symbols"
 )
 
+// Segment contains the information required to render a segment for the prompt
 type Segment struct {
 	Text       string
 	Foreground color.Color
@@ -14,42 +15,50 @@ type Segment struct {
 	Bold       bool
 }
 
+// EndSegment resets the color and formatting to default at the end of the prompt
 var EndSegment = Segment{
 	Foreground: color.Default,
 	Background: color.Default,
 }
 
+// Render takes a list of segments and renders them into a prompt string
 func Render(segs ...Segment) string {
 
 	if len(segs) == 0 {
 		return ""
 	}
 
+	// use a string builder to speed up the process
 	buf := &strings.Builder{}
 
+	// the first segment is just rendered as-is with no starting separator
 	buf.WriteString(color.Render(segs[0].Foreground, segs[0].Background))
 	buf.WriteString(segs[0].Text)
 
 	for i, s := range segs[1:] {
 
+		// render the separator
 		if segs[i].Background == s.Background {
-			// Both backgrounds are the same, render just a thin sep
+			// if both backgrounds are the same color, render just a thin separator
 			buf.WriteRune(symbols.SepThinRight)
 		} else {
 			buf.WriteString(color.Render(segs[i].Background, s.Background))
 			buf.WriteRune(symbols.SepRight)
 		}
 
+		// add the formatting to be used, written like this with extensibility in mind
 		var fmts []color.Formatting
 		if s.Bold {
 			fmts = append(fmts, color.Bold)
 		}
 
+		// render the segment text
 		buf.WriteString(color.Render(s.Foreground, s.Background, fmts...))
 		buf.WriteString(s.Text)
 
 	}
 
+	// turn the buffer into a string and return it
 	return buf.String()
 
 }
