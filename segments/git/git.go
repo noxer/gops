a342d9a13eb4bd103c601ee4c4708581b6b95bb7
Tim Scheuermann <tim@plusmid.net>
2018-12-01 18:03:24 +0100

    Comments for the git package

--- segments/git/git.go
+++ segments/git/git.go
@@ -5,33 +5,40 @@ import (
 
 	"github.com/noxer/gops/color"
 	"github.com/noxer/gops/separator"
+	"github.com/noxer/gops/symbols"
 	"gopkg.in/src-d/go-git.v4"
 )
 
+// Add the Git branch as a segment
 func Add(segs []separator.Segment) []separator.Segment {
 
+	// get the current directory
 	wd, err := os.Getwd()
 	if err != nil {
 		return segs
 	}
 
+	// search for a Git repo to we may be in
 	repo, err := git.PlainOpenWithOptions(wd, &git.PlainOpenOptions{DetectDotGit: true})
 	if err != nil {
 		return segs
 	}
 
+	// use the HEAD of the repo to find the current branch
 	head, err := repo.Head()
 	if err != nil {
+		// no HEAD in this repo, must be empty
 		return append(segs, separator.Segment{
-			Text:       " \ue0a0 ? ",
+			Text:       " " + string(symbols.Branch) + " ? ",
 			Foreground: color.DarkGray,
 			Background: color.Yellow,
 			Bold:       true,
 		})
 	}
 
+	// add the segment with the repo name
 	s := separator.Segment{
-		Text:       " \ue0a0 " + head.Name().Short() + " ",
+		Text:       " " + string(symbols.Branch) + " " + head.Name().Short() + " ",
 		Foreground: color.DarkGray,
 		Background: color.Yellow,
 		Bold:       true,
